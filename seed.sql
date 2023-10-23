e18da25ab7de7186d214fe662d5dc362026ba2fd
hrvadl <vhrashch@cisco.com>
2023-10-23 09:37:13 +0300

    feat(backend): updated seed.sql

--- seed.sql
+++ seed.sql
@@ -2,3 +2,4 @@ INSERT INTO `securities` (id, name) VALUES (1,'Security 1'),(2,'Security 2'),(3,
 
 INSERT INTO `users` VALUES (1,'Hrashchenko','stock','Vadym','test@test.com','$2a$10$.e8t6tmhMv5ZwLOIZsEaH.1InvpZcOOZ3RH6IiPv..N9SqlBpQEGG',1000);
 
+INSERT INTO `users` VALUES (2,'Hrashchenko','stock','Vadym','test1@test.com','$2a$10$.e8t6tmhMv5ZwLOIZsEaH.1InvpZcOOZ3RH6IiPv..N9SqlBpQEGG',1000);
